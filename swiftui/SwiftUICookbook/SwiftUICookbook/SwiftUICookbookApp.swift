da025492218d9e18296d316f60090d4de69ff0e9
Brent Crowley <brent.crowley@seventhbeam.com>
2023-10-03 16:00:57 +1100

    add swiftui and uikit blank project to populate with examples of code snippets

---
+++ swiftui/SwiftUICookbook/SwiftUICookbook/SwiftUICookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICookbookApp.swift
+//  SwiftUICookbook
+//
+//  Created by Brent Crowley on 3/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ uikit/UIKitCookbook/UIKitCookbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitCookbook
+//
+//  Created by Brent Crowley on 3/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
